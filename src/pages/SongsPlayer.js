d8d6deac8161fd0fd1c2bf752c3aeb6eaf2edb9f
Siddharth <1928321@kiit.ac.in>
2025-08-01 17:25:23 +0530

    Removed audio song

--- src/pages/SongsPlayer.js
+++ src/pages/SongsPlayer.js
@@ -4,21 +4,25 @@ function SongsPlayer() {
   return (
     <div className="songs-container">
       <h1 className="songs-title">🎶 Song for you</h1>
-      <p className="songs-subtitle">"A melody that plays only for you, Mehak 💖"</p>
-
-      <div className="player-wrapper">
-        <audio controls>
-          <source src={require('../assets/our_song.mp3')} type="audio/mp3" />
-          Your browser does not support the audio element.
-        </audio>
-      </div>
 
       <div className="lyrics-box">
         <p>
-          I found you in chaos, in moments unclear, <br />
-          And suddenly, the world felt near. <br />
-          Your laugh, your smile, your magic touch, <br />
-          Oh Mehak, you mean so much. 💗
+          Jeens top chodha,<br />
+          Odha math pe chunariya.<br />
+          Jaibu piya ke ghare ban ke bahuriya.<br />
+          Chodha dilli bambai ke luur ho,
+          Chala bhojpur hoo.<br />
+          Tani tani baatwa ke ho gainin majboor ho,<br />
+          Tani tani baatwa ke ho gainin majboor ho.<br />
+          Tohar singar ke ba ek ta suroor ho.<br />
+          Na lauke la sona,<br />
+          Na chaani laukat ho.<br />
+          Na lauke la sona,<br />
+          Na chaani laukat ho.<br />
+          Ajma le hmra tohar khatir sab bisar deb ho.<br />
+          Chala ho gail samay ab,<br />
+          Apan chale ka bakhat ba,<br />
+          Chala bhojpur hoo.<br />
         </p>
       </div>
     </div>
